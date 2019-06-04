d45d431a21e6ecddd61a78b0d2cca93df98f9e76
Gilda Snow <gilda.snow@gmail.com>
2019-06-04 19:08:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,5 @@ def line(deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each_with_index do 
+    deli.each_with_index do |index, i|
+      
