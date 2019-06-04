566100bd37565e709de2a41c14cbcc6b16cabf39
Gilda Snow <gilda.snow@gmail.com>
2019-06-04 19:11:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,4 +4,9 @@ def line(deli)
   else
     current_line = "The line is currently:"
     deli.each_with_index do |person, i|
-      
+      current_line << " #{i}. #{person}"
+    end
+  end
+end
+
+    
